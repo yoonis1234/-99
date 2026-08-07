91201f7a14e0510cc325183f2bd38d2433cc8ac7
yoonis1234 <cadepoi32@gmail.com>
2026-08-08 01:12:54 +0300

    Fetch and log data from API in script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-console.log("/99 Dashboard loaded ✅");
-
-function welcome() {
-    alert("Welcome to /99 Dashboard ⚡");
-}
+fetch("http://localhost:3000/stats")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log("API Error:", error);
+    });
